592da646d1c5efb036434746802b2cb93a00530c
TheBurningDude <nasibo2005@hotmail.com>
2016-05-09 00:46:44 +0200

    Health drawn

--- UserInterface/src/main/java/dk/sdu/mmmi/cbse/userinterface/UIEntityPlugin.java
+++ UserInterface/src/main/java/dk/sdu/mmmi/cbse/userinterface/UIEntityPlugin.java
@@ -22,34 +22,38 @@ public class UIEntityPlugin implements IGamePluginService {
     private Map<String, Entity> world;
     private Entity health;
     private int pos = 0;
+    private int ID = 1;
 
     @Override
     public void start(GameData gameData, Map<String, Entity> world) {
         this.world = world;
         // Add entities to the world
         for (int i = 0; i < 3; i++) {
-            health = createHealth(gameData);
+            health = createHealth();
             world.put(health.getID(), health);
             pos = pos + 15;
+            ID = ID + 1;
         }
 
     }
 
-    private Entity createHealth(GameData gameData) {
+    private Entity createHealth() {
         Entity healthEntity = new Entity();
         healthEntity.setType(EntityType.UI);
         healthEntity.setPosition(50 + pos, 550);
         healthEntity.setShapeX(new float[4]);
         healthEntity.setShapeY(new float[4]);
         healthEntity.setRadians(3.1415f / 2);
-        System.out.println(pos);
+        System.out.println(ID);
         return healthEntity;
 
     }
 
     @Override
     public void stop(GameData gameData) {
-        world.remove(health.getID());
+        
+            world.remove(health.getID());
+        
     }
 
 }
